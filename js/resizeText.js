3b6fc8f396eedc01a678524e51b33189844bd829
vsfrutos <43183440+vsfrutos@users.noreply.github.com>
2019-01-14 11:59:54 +0100

    test

--- js/resizeText.js
+++ js/resizeText.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-var resizeText = function () {
-        // Predefinido tamaño de letra que encaje con esas dimensiones
-        var predefinidoTamanoFuente = 200; // %
-        var predefinidoResolucion = 1024;
-
-        var tamanoActual = $(window).width(); // Ancho de la ventana
-        var porcentaje = Math.sqrt(tamanoActual) / Math.sqrt(predefinidoResolucion);
-        var nuevoTamanoFuente = predefinidoTamanoFuente * porcentaje;
-        $("#acercade h1").css("font-size", nuevoTamanoFuente + '%');
-        $("#acercade p").css("font-size", nuevoTamanoFuente / 2+ '%');
-        $("#intro-text h1").css("font-size", nuevoTamanoFuente + '%');
-        $("#intro-text h3").css("font-size", nuevoTamanoFuente / 2 + '%');
-        $("#main-menu li:not(:last-child)").css("font-size", nuevoTamanoFuente / 2 + '%');
-        $(".phone").css("font-size", nuevoTamanoFuente / 2 + '%');
-
-      };
-
-      $(document).ready(function() {
-              resizeText(); //ASIGNAR DIMENSIONES
-
-      });
-
-      $(window).resize(function() {
-          resizeText(); //ACTUALIZAR DIMENSIONES
-
-
-      });
-=======
 var resizeText = function () {
         // Predefinido tamaño de letra que encaje con esas dimensiones
         var predefinidoTamanoFuente = 200; // %
@@ -52,4 +23,3 @@ var resizeText = function () {
 
 
       });
->>>>>>> 0161d0250a41ce2230b53dfe6fee6b84066ce558
